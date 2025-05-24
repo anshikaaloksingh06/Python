8f3506bef666cb34054c3913e7270d3166ae8042
anshikaaloksingh06 <anshikaaa.24.beai@acharya.ac.in>
2025-05-24 22:16:09 +0530

    Create CharacterPictureGrid.py

---
+++ CharacterPictureGrid.py
@@ -0,0 +1,8 @@
+print("Name: Anshikaa A Singh")
+print("USN: 1AY24AI011")
+print("Section: M")
+grid_size = int(input("Enter the size of the grid: "))
+for i in range(grid_size):
+    for j in range(grid_size):
+        print("*", end=" ")
+    print()
