f489161c225138be8a8dca713a4df2c4b73a46d6
anshikaaloksingh06 <anshikaaa.24.beai@acharya.ac.in>
2025-05-24 22:49:19 +0530

    Create TablePrinter.py

---
+++ TablePrinter.py
@@ -0,0 +1,6 @@
+print("Name: Anshikaa A Singh")
+print("USN: 1AY24AI011")
+print("Section: M")
+number = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
